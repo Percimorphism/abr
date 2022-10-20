4f494fda301d5ca8397caa5fb3bbdeb3bc91098f
Percimorphsim <Percimorphism@users.noreply.github.com>
2022-10-20 10:49:21 -0700

    abr heatmap

---
+++ main_page.py
@@ -0,0 +1,9 @@
+# Contents of ~/my_app/streamlit_app.py
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.title("American Bike Ride Ridership Zipcode 2022 🚲 ")
+
---
+++ pages/dc.py
@@ -0,0 +1,34 @@
+import imp
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pgeocode
+
+
+st.title("DC Ridership Heatmap")
+
+DATA_URL=("./datasource/dcbr-zip-codes.xlsx")
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_excel(DATA_URL, sheet_name='complete', engine='openpyxl')
+    return data
+
+philly = load_data()
+
+nomi = pgeocode.Nominatim('US')
+loc = nomi.query_postal_code((philly['Zip'].astype(str)).to_list())
+# st.write(loc)
+
+heatmap_data = {'zip': loc['postal_code'],
+                'lat': loc['latitude'], 
+                'lon' : loc['longitude']} 
+hm = pd.DataFrame(data=heatmap_data) 
+hm = hm.dropna(how='any')
+# st.write(hm)
+
+st.map(hm[['lat', 'lon']])
+
+
+
