6a49fc71c89a40609595c9e2243d2b38d23dd961
Percimorphsim <Percimorphism@users.noreply.github.com>
2023-02-24 11:23:19 -0800

    added philly field marketing

---
+++ pages/phillyfieldmarketing2023.py
@@ -0,0 +1,36 @@
+import imp
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pgeocode
+
+
+st.title("Philly 2023 Field Marketing Heatmap")
+
+DATA_URL=("./datasource/Field Marketing Philly Zips.xlsx")
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_excel(DATA_URL, engine='openpyxl')
+    data['Zip'] = (data['Zip'].astype(str)).str[:5]
+    return data
+
+pfm = load_data()
+st.write(pfm)
+
+nomi = pgeocode.Nominatim('US')
+loc = nomi.query_postal_code((pfm['Zip'].astype(str)).to_list())
+
+heatmap_data = {'zip': loc['postal_code'],
+                'lat': loc['latitude'], 
+                'lon' : loc['longitude']} 
+hm = pd.DataFrame(data=heatmap_data) 
+
+hm = hm.dropna(how='any')
+# st.write(hm)
+
+st.map(hm[['lat', 'lon']])
+
+
+
